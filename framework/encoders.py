27171107a9fd33e9f4d2339c4196db794271f108
thomas475 <developer42@gmx.de>
2022-04-03 10:04:43 +0200

    modified custom encoders for direct use in sklearn.pipeline

--- framework/encoders.py
+++ framework/encoders.py
@@ -35,32 +35,62 @@ class TargetEncoder(Encoder):
     """
     Maps categorical values into the average target associated to them.
     """
+
     def __init__(self, default=-1, **kwargs):
         super().__init__(default=default, **kwargs)
 
     def fit(self, X: pd.DataFrame, y, **kwargs):
-        self.cols = X.columns
-        target_name = y.name
-        X = X.join(y.squeeze())
+        X = X.copy()
+
+        # convert column names to a more practical form
+        column_titles = range(0, len(X.columns))
+        X.columns = column_titles
+
+        # adding target vector
+        target_name = len(X.columns)
+        X[target_name] = y
+
+        # selecting categorical columns for processing
+        self.cols = list(X.select_dtypes(['object']).columns)
+
         for col in self.cols:
             temp = X.groupby(col)[target_name].mean().to_dict()
             self.encoding[col].update(temp)
+
         return self
 
     def transform(self, X: pd.DataFrame, y=None, **kwargs):
         X = X.copy()
+
+        # convert column names to a more practical form
+        original_column_titles = X.columns
+        X.columns = range(0, len(X.columns))
+
         for col in self.cols:
             X[col] = X[col].apply(lambda cat: self.encoding[col][cat])
+
+        # reverting to original column names
+        X.columns = original_column_titles
+
         return X
 
 
 class CollapseEncoder(Encoder):
     """
-    Evey categorical value is mapped to 1.
+    Every categorical value is mapped to 1.
     """
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def transform(self, X: pd.DataFrame, y=None, **kwargs):
-        self.cols = X.columns
-        return pd.DataFrame(np.ones(len(X)), index=X.index, columns=['cat'])
+        X = X.copy()
+
+        # selecting categorical columns for processing
+        self.cols = list(X.select_dtypes(['object']).columns)
+
+        # drop all categorical columns and add a column instead with the value 1 everywhere
+        X = X.drop(columns=self.cols)
+        X = pd.concat([X, pd.Series(np.ones(len(X)), index=X.index, name='cat')], axis=1)
+
+        return X
--- tests/test_encoders.py
+++ tests/test_encoders.py
@@ -1,10 +1,11 @@
+import numpy as np
+import pandas as pd
+
+from sklearn import datasets
+from sklearn import tree
+
 import category_encoders as ce
-from sklearn.preprocessing import RobustScaler
-
 import framework.encoders as enc
-
-from framework.encoders import *
-from framework.samplers import *
 
 encoders = [
     ce.BackwardDifferenceEncoder(),
@@ -13,7 +14,7 @@ encoders = [
     ce.CatBoostEncoder(),
     ce.CountEncoder(),
     ce.GLMMEncoder(),
-    ce.HashingEncoder(),
+    #ce.HashingEncoder(),
     ce.HelmertEncoder(),
     ce.JamesSteinEncoder(),
     ce.LeaveOneOutEncoder(),
@@ -29,12 +30,15 @@ encoders = [
     enc.CollapseEncoder()
 ]
 
-scaler = RobustScaler()
+adult = pd.read_csv('../data/adult.csv')
+X = adult.drop(columns='income')
+y = adult['income'].map({'<=50K': 0, '>50K': 1})
 
-samplers = [
-    ProportionalSMOTESampler(),
-    UnlabeledSMOTESampler(),
-    ProportionalRACOGSampler(),
-    UnlabeledRACOGSampler(),
-    UnlabeledVanillaGANSampler()
-]
+start = 98
+end = start + 5
+X = X.loc[start:end - 1, :]
+y = y[start:end]
+
+for encoder in encoders:
+    print(encoder)
+    print(encoder.fit_transform(X, y))
